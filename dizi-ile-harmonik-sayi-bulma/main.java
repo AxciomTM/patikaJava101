4d5fa4a03c2fc908dc528d908843c5da02ba7a6c
Kaan <108834287+AxciomTM@users.noreply.github.com>
2022-11-08 22:49:45 +0300

    Create main.java

---
+++ dizi-ile-harmonik-sayi-bulma/main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner girdi = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = girdi.nextInt();
+        double toplam = 0;
+        double [] sayilar = new double [sayi];
+
+        for (int i = 0; i < sayilar.length; i++) {
+            sayilar[i] = (double)(1)/(i+1);
+        }
+        for (int i = 0; i < sayilar.length; i++) {
+            toplam += sayilar[i];
+        }
+        System.out.println(toplam);
+
+    }
+}
